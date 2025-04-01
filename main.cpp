3ba46ff6cf11bc2d2b90d6a55e3da07442100e2d
eovywjr1 <eovywjr1@naver.com>
2025-04-01 09:10:46 +0900

    정수, 실수 사칙연산

--- main.cpp
+++ main.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+void print(float num1, float num2)
+{
+
+}
+
+
 int main()
 {
-	cout << "Hello World" << endl;
+	int num1 = 1, num2 = 2;
+
+	cout << "int variable print" << endl;
+	cout << num1 + num2 << endl;
+	cout << num1 - num2 << endl;
+	cout << num1 / num2 << endl;
+	cout << num1 * num2 << endl;
+	cout << num1 % num2 << endl;
+
+	float num3 = 1.5f, num4 = 2.5f;
 	
+	cout << "float var4able print" << endl;
+	cout << num3 + num4 << endl;
+	cout << num3 - num4 << endl;
+	cout << num3 / num4 << endl;
+	cout << num3 * num4 << endl;
+			   
 	return 0;
 }
